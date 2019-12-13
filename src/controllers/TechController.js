a08e6f5e59692201553bf2afb46ad67a82e2d17b
nubelsondev <nubelsondev@hotmail.com>
2019-12-13 15:57:39 +0000

    add the controller to delete the relationship

--- src/controllers/TechController.js
+++ src/controllers/TechController.js
@@ -24,5 +24,22 @@ module.exports = {
         await user.addTech(tech);
 
         return res.json(tech);
+    },
+
+    async delete(req, res) {
+        const { user_id } = req.params;
+        const { name } = req.body;
+
+        const user = await User.findByPk(user_id);
+
+        !user && res.status(400).json({ error: "User not found!" });
+
+        const tech = await Tech.findOne({
+            where: { name }
+        });
+
+        await user.removeTech(tech);
+
+        return res.json();
     }
 };
